f8b06663dec973524603a8c1a876f4e236967568
JorgeVeraD1234 <verad@JorgeVera04>
2026-03-18 23:20:15 -0600

    Vamos

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,8 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # backend/app/config.py
+""" 
+"postgresql://postgres:TfjOAypcGyngOQbUVmsNFozjmqyYxaVp@switchback.proxy.rlwy.net:58066/railway"
+
+"""
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:TfjOAypcGyngOQbUVmsNFozjmqyYxaVp@switchback.proxy.rlwy.net:58066/railway"
+    SQLALCHEMY_DATABASE_URI ="postgresql://postgres:TfjOAypcGyngOQbUVmsNFozjmqyYxaVp@switchback.proxy.rlwy.net:58066/railway"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "algo_seguro"
     JWT_SECRET_KEY = "algo_seguro"
